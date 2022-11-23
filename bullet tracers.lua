1a6bcc8c12a191e16ae110f530a0e0ca809818ed
Bo <bo.van.laarhoven@gmail.com>
2022-11-23 21:34:18 +0100

    Delete bullet tracers.lua

--- bullet tracers.lua
+++
@@ -1,7 +0,0 @@
-local highlight = Instance.new("Highlight")
-
-game:GetService("Workspace").DescendantAdded:Connect(function(descendant)
-	if descendant:IsA("Part") and descendant.Name == "newBullet" then
-        highlight.Parent = descendant
-	end
-end)
