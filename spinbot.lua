093060d3bff87d15a6e816cc9619828d2b2c5d8f
Bo <bo.van.laarhoven@gmail.com>
2022-11-27 23:32:07 +0100

    Update spinbot.lua

--- spinbot.lua
+++ spinbot.lua
@@ -7,27 +7,27 @@ local Window = Rayfield:CreateWindow({
 	LoadingSubtitle = "by Sirius",
 	ConfigurationSaving = {
 		Enabled = true,
-		FolderName = nil, -- Create a custom folder for your hub/game
+		FolderName = nil,
 		FileName = "Big Hub"
 	},
         Discord = {
         	Enabled = false,
-        	Invite = "sirius", -- The Discord invite code, do not include discord.gg/
-        	RememberJoins = true -- Set this to false to make them join the discord every time they load it up
+        	Invite = "sirius",
+        	RememberJoins = true 
         },
-	KeySystem = false, -- Set this to true to use our key system
+	KeySystem = false,
 	KeySettings = {
 		Title = "Sirius Hub",
 		Subtitle = "Key System",
 		Note = "Join the discord (discord.gg/sirius)",
 		FileName = "SiriusKey",
 		SaveKey = true,
-		GrabKeyFromSite = false, -- If this is true, set Key below to the RAW site you would like Rayfield to get the key from
+		GrabKeyFromSite = false,
 		Key = "Hello"
 	}
 })
 
-local Tab = Window:CreateTab("Tab Example", 4483362458) -- Title, Image
+local Tab = Window:CreateTab("Tab Example", 4483362458)
 
 function spin()
 	local Spin = Instance.new("BodyAngularVelocity")
@@ -39,7 +39,7 @@ end
 
 local Slider = Tab:CreateSlider({
 	Name = "SpinSpeed",
-	Range = {0, 100},
+	Range = {0, 500},
 	Increment = 10,
 	Suffix = "Bananas",
 	CurrentValue = 10,
