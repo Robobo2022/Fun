a9527e64c80640b9ea8346d8c844ff64a8d77a3a
Bo <bo.van.laarhoven@gmail.com>
2022-12-07 17:55:22 +0100

    Update spinbot.lua

--- spinbot.lua
+++ spinbot.lua
@@ -1,9 +1,10 @@
-getgenv().spinSpeed = 20
+local char = game:GetService("Players").LocalPlayer.Character
+local humanoid = char.Humanoid
 local Spin = Instance.new("BodyAngularVelocity")
 local Rayfield = loadstring(game:HttpGet('https://raw.githubusercontent.com/shlexware/Rayfield/main/source'))()
 local Window = Rayfield:CreateWindow({
-	Name = "Spinbot",
-	LoadingTitle = "Spinbot (op)",
+	Name = "Spinbot/bhop",
+	LoadingTitle = "Spinbot/bhop",
 	LoadingSubtitle = "by hydra#8270",
 	ConfigurationSaving = {
 		Enabled = true,
@@ -27,7 +28,20 @@ local Window = Rayfield:CreateWindow({
 	}
 })
 
-local Tab = Window:CreateTab("Tab Example", 4483362458) 
+getgenv().spinSpeed = 20
+getgenv().bhop = false
+getgenv().bhopwait = 1
+
+local T1 = Window:CreateTab("SpinBot", 4483362458) 
+local T2 = Window:CreateTab("Bhop", 4483362458) 
+
+task.spawn(function()
+	while task.wait(bhopwait) do
+		if bhop then
+			humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+		end
+	end
+end)
 
 function spin()
 	local Spin = Instance.new("BodyAngularVelocity")
@@ -37,7 +51,7 @@ function spin()
 	Spin.AngularVelocity = Vector3.new(0,spinSpeed,0)
 end
 
-local Slider = Tab:CreateSlider({
+local Slider = T1:CreateSlider({
 	Name = "SpinSpeed",
 	Range = {0, 500},
 	Increment = 10,
@@ -49,7 +63,7 @@ local Slider = Tab:CreateSlider({
 	end,
 })
 
-local Toggle = Tab:CreateToggle({
+local Toggle = T1:CreateToggle({
 	Name = "Spin-bot",
 	CurrentValue = false,
 	Flag = "Toggle1",
@@ -59,5 +73,26 @@ local Toggle = Tab:CreateToggle({
 		else
 			game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Spinning:Destroy()
 		end
+	end,
+})
+
+local Slider = T2:CreateSlider({
+	Name = "Bhop Delay",
+	Range = {0.5, 50},
+	Increment = 0.1,
+	Suffix = "Delay",
+	CurrentValue = 1,
+	Flag = "Slider1",
+	Callback = function(Value)
+		bhopwait = Value
+	end,
+})
+
+local Toggle = T2:CreateToggle({
+	Name = "Bhop",
+	CurrentValue = false,
+	Flag = "Toggle1",
+	Callback = function(Value)
+		bhop = Value
 	end,
 })
