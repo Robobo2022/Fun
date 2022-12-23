cdd2991b5cc67d18b6ac1322e142cf5a5eb02c94
Bo <bo.van.laarhoven@gmail.com>
2022-12-23 14:21:35 +0100

    Update MM2.lua

--- MM2.lua
+++ MM2.lua
@@ -39,6 +39,7 @@ task.spawn(function()
                     CFrameEnd = CFrame.new(v.Position)
                     local tween = game:GetService("TweenService"):Create(game.Players.LocalPlayer.Character.HumanoidRootPart, TweenInfo.new(Time), {CFrame = CFrameEnd})
                     tween:Play()
+                    tween.Completed:Wait()
                     if AutoFarm == false then
                         tween:Cancel()
                     end
