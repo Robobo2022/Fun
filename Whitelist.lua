1897731226e3d56dd67829a641cc2b31c0409954
Bo <bo.van.laarhoven@gmail.com>
2022-12-27 18:02:16 +0100

    Update Whitelist.lua

--- Whitelist.lua
+++ Whitelist.lua
@@ -7,29 +7,44 @@ local Decoded = game:GetService('HttpService'):JSONDecode(Body)
 local GetHwid = Decoded.headers["Sw-Fingerprint"];
 local GAMENAME = game:GetService("MarketplaceService"):GetProductInfo(game.PlaceId).Name
 local Time = os.clock()
-print("[Whitelist] Loading...")
-wait(0.2)
-print("[Whitelist] ".. identifyexecutor())
-wait(0.2)
 
-print("[Whitelist] Checking Whitelist...")
-
-
-if (writefile) then
-    json = HttpService:JSONEncode({["hwid"] = GetHwid})
-    writefile(filename, json)
+_G.Settings = {
+    ["key"] = _G.Key,
+    ["site"] = "https://yoursite.com/whitelist.php?key=",
+    ["text"] = "Youre not whitelisted please contact our staff/owner";
+}
+local abc = game:HttpGet(_G.Settings.site .. _G.Settings.key)
+if abc == "true" then
+    print("[Whitelist] Loading...")
+    wait(0.2)
+    print("[Whitelist] ".. identifyexecutor())
+    wait(0.2)
+    
+    print("[Whitelist] Checking Whitelist...")
+    
+    if (writefile) then
+        json = HttpService:JSONEncode({["hwid"] = GetHwid})
+        writefile(filename, json)
+    else
+        json = readfile(filename)
+    end
+    
+    print("[Whitelist] Took: " .. math.round(os.clock() - Time) .. "s")
+    
+    if (not json) then
+        print("[Whitelist] Failed to load whitelist.")
+        return
+    end
+    
+    if (not HttpService:JSONDecode(json)) then
+        print("[Whitelist] Failed to load whitelist.")
+        return
+    end
+elseif abc == "false" then
+game.Players.LocalPlayer:Kick(_G.Settings.text)
 else
-    json = readfile(filename)
+    print("noob")
 end
 
-print("[Whitelist] Took: " .. math.round(os.clock() - Time) .. "s")
 
-if (not json) then
-    print("[Whitelist] Failed to load whitelist.")
-    return
-end
 
-if (not HttpService:JSONDecode(json)) then
-    print("[Whitelist] Failed to load whitelist.")
-    return
-end
