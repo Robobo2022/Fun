be1b816237b94e3e4925bcf85e4487aabcb1b975
Bo <bo.van.laarhoven@gmail.com>
2022-12-27 17:11:35 +0100

    Update MM2.lua

--- MM2.lua
+++ MM2.lua
@@ -68,3 +68,66 @@ local Slider = Tab:CreateSlider({
 })
 
 
+task.spawn(function()
+	while task.wait(bhopwait) do
+		if bhop then
+			humanoid:ChangeState(Enum.HumanoidStateType.Jumping)
+		end
+	end
+end)
+
+function spin()
+	local Spin = Instance.new("BodyAngularVelocity")
+	Spin.Name = "Spinning"
+	Spin.Parent = game:GetService("Players").LocalPlayer.Character.HumanoidRootPart
+	Spin.MaxTorque = Vector3.new(0, math.huge, 0)
+	Spin.AngularVelocity = Vector3.new(0,spinSpeed,0)
+end
+
+local Slider = Tab:CreateSlider({
+	Name = "SpinSpeed",
+	Range = {0, 500},
+	Increment = 10,
+	Suffix = "Speed",
+	CurrentValue = 1,
+	Flag = "Slider1",
+	Callback = function(Value)
+		spinSpeed = Value
+	end,
+})
+
+local Toggle = Tab:CreateToggle({
+	Name = "Spin-bot",
+	CurrentValue = false,
+	Flag = "Toggle1",
+	Callback = function(Value)
+		if Value then
+			spin()
+		else
+			game:GetService("Players").LocalPlayer.Character.HumanoidRootPart.Spinning:Destroy()
+		end
+	end,
+})
+
+local Slider = Tab:CreateSlider({
+	Name = "Bhop Delay",
+	Range = {1, 50},
+	Increment = 0.1,
+	Suffix = "Delay",
+	CurrentValue = 1,
+	Flag = "Slider1",
+	Callback = function(Value)
+		bhopwait = Value
+	end,
+})
+
+local Toggle = Tab:CreateToggle({
+	Name = "Bhop",
+	CurrentValue = false,
+	Flag = "Toggle1",
+	Callback = function(Value)
+		bhop = Value
+	end,
+})
+
+
